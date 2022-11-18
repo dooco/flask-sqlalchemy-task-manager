ea95f8461aeb3dc554559db98597eae8b991e823
dooco <padraigdooley@gmail.com>
2022-11-18 08:46:23 +0000

    add read function to code

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,11 +2,10 @@ from taskmanager import db
 
 
 class Category(db.Model):
-    #  schema for Category model
+    # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-    tasks = db.relationship(
-        "Task", backref="category", cascade="all, delete", lazy=True)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
@@ -14,14 +13,13 @@ class Category(db.Model):
 
 
 class Task(db.Model):
-    # schema for Task model
+    # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    Category_id = db.Column(db.Integer, db.ForeignKey(
-        "category.id", ondelete="CASCADE"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
