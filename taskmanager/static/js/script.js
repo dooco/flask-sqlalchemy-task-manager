7a35bbdc3b3dcb96c02055ffcd79b99210130f0e
dooco <padraigdooley@gmail.com>
2022-11-18 22:14:12 +0000

    Adding task function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,21 @@
+const {
+    Datepicker
+} = require("materialize-css");
+
 document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
-    console.log("script loaded");
+
+    // Datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
